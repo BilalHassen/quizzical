34e5f6627b7e08311903fca750563087ed66249f
Bilal Hassen <bilalhassen097@gmail.com>
2025-10-01 23:08:21 -0400

    Add example components and fix SCSS variable issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Button from './components/Button';
+
 function App() {
   return (
     <div className="app">
-      {/* Your app content goes here */}
+      <Header 
+        title="Quizzical" 
+        subtitle="Test your knowledge with our quiz app" 
+      />
+      <div className="app__content">
+        <Button variant="primary" onClick={() => console.log('Start Quiz!')}>
+          Start Quiz
+        </Button>
+        <Button variant="secondary" onClick={() => console.log('View Results')}>
+          View Results
+        </Button>
+      </div>
     </div>
   )
 }
