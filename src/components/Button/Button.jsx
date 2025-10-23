19082c6acf21799e5b40a471fb544dad7728e577
Bilal Hassen <bilalhassen097@gmail.com>
2025-10-22 23:59:35 -0400

    update styling and component improvements

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,24 +4,16 @@ import he from 'he';
 
 function Button({ text, onClick, isSelected, isDisabled, isCorrect, isGameDone }) {
 
-
-
-
-
   return (
-    <>
     <button
       onClick={onClick}
       className={`answer-button ${isSelected ? 'answer-button--selected' : ''}
       ${isSelected && isGameDone ? (isCorrect ? "correct" : "incorrect") : null}
-      ${isGameDone && !isSelected ? "blurred" : null}
-
-      `}
+      ${isGameDone && !isSelected ? "blurred" : null}`}
       disabled={isDisabled}
     >
       {he.decode(text)}
     </button>
-    </>
   );
 }
 
